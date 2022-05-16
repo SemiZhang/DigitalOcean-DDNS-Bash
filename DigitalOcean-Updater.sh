122b8e14a2293378fb8023f2de9401b84be52b6a
SemiZhang <shunyang.zhang@epfedu.fr>
2022-05-16 13:52:02 +0200

    Remove dependency on json_pp
    No more dependency on 'json_pp'
    Handled by 'grep' and 'awk'

--- DigitalOcean-Updater.sh
+++ DigitalOcean-Updater.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#https://github.com/SemiZhang/DigitalOcean-DDNS-Bash
+#v1.1
+
 #################### VARIABLES ####################
 TOKEN="aaaaabbbbbccccc000001111122222"
 DOMAIN="example.com"
@@ -12,12 +15,12 @@ CURRENT_IPV6="$(curl -s v6.ident.me)"
 
 for RECORD_ID in ${RECORD_V4[*]}
 do
-	LAST_IPV4="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"  | json_pp | grep "data" | awk -F '"' '{print $4}')"
-	HOST="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"  | json_pp | grep "name" | awk -F '"' '{print $4}')"
+	LAST_IPV4="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"  | grep -Po '"data":(\d*?,|.*?[^\\]",)' | awk -F '"' '{print $4}')"
+	HOST="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"  | grep -Po '"name":(\d*?,|.*?[^\\]",)' | awk -F '"' '{print $4}')"
 	if [ $CURRENT_IPV4 == $LAST_IPV4 ]; then
-		echo "IPV4 has not changed for [$HOST] ($CURRENT_IPV4)"
+		echo "IPv4 has not changed for [$HOST] ($CURRENT_IPV4)"
 	else
-		echo "IPV4 for [$HOST] has changed to $CURRENT_IPV4 from $LAST_IPV4"
+		echo "IPv4 for [$HOST] has changed to $CURRENT_IPV4 from $LAST_IPV4"
 		curl -s -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"data":"'"$CURRENT_IPV4"'"}' "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"
 		echo ""
 	fi
@@ -25,12 +28,12 @@ done
 
 for RECORD_ID in ${RECORD_V6[*]}
 do
-	LAST_IPV6="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"  | json_pp | grep "data" | awk -F '"' '{print $4}')"
-	HOST="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"  | json_pp | grep "name" | awk -F '"' '{print $4}')"
+	LAST_IPV6="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"  | grep -Po '"data":(\d*?,|.*?[^\\]",)' | awk -F '"' '{print $4}')"
+	HOST="$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"  | grep -Po '"name":(\d*?,|.*?[^\\]",)' | awk -F '"' '{print $4}')"
 	if [ $CURRENT_IPV6 == $LAST_IPV6 ]; then
 		echo "IPv6 has not changed for [$HOST] ($CURRENT_IPV6)"
 	else
-		echo "IPV6 for [$HOST] has changed to $CURRENT_IPV6 from $LAST_IPV6"
+		echo "IPv6 for [$HOST] has changed to $CURRENT_IPV6 from $LAST_IPV6"
 		curl -s -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"data":"'"$CURRENT_IPV6"'"}' "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"
 		echo ""
 	fi
